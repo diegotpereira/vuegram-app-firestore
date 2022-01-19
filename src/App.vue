507140da457a5751d9902afdbf92425b4fdf54cd
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-19 17:11:24 -0300

    alteração no arquivo app vue.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <SiteNav v-if="showNav"></SiteNav>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import { mapState } from 'vuex'
+import SiteNav from '@/components/SiteNav'
+export default {
+    components: {
+        SiteNav
+    },
+    computed: {
+        ...mapState(['userProfile']),
+        showNav() {
+            return Object.keys(this.userProfile).length > 1
+        }
+    }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
